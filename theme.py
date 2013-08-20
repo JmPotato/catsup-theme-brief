531d4a29a4d73461aa5f8598801adc245e0bc846
JmPotato <ghzpotato@gmail.com>
2013-08-20 21:16:04 +0800

    Update theme.py

--- theme.py
+++ theme.py
@@ -4,18 +4,5 @@ homepage = 'https://github.com/JmPotato/catsup-theme-brief'
 post_per_page = 3
 vars = {
     "imge_path": "http://farm4.staticflickr.com/3752/9436685808_ff3fd6be7c_m.jpg",
-    "description": "a blog",
-    "github": "JmPotato",
-    "links": [
-        {
-            "name": "catsup",
-            "url": "https://github.com/whtsky/catsup",
-            "description": "Awesome!"
-        }
-        {
-            "name": "Brief",
-            "url": "https://github.com/JmPotato/catsup-theme-brief",
-            "description": "The theme for catsup."
-        }
-    ]
+    "description": "a blog"
 }
