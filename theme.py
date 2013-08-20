ccc5e2ad25c886c477321613f71040d7f80045a5
JmPotato <ghzpotato@gmail.com>
2013-08-20 17:58:59 +0800

    Add Some Files

---
+++ theme.py
@@ -0,0 +1,15 @@
+name = 'sealscript'
+author = 'Lyric'
+homepage = 'https://github.com/whtsky/catsup-theme-sealscript'
+post_per_page = 3
+vars = {
+    "description": "a blog",
+    "github": "whtsky",
+    "links": [
+        {
+            "name": "catsup",
+            "url": "https://github.com/whtsky/catsup",
+            "description": "Awesome!"
+        }
+    ]
+}
