b2151adf04b5329d4466b0f0dd5b698f360e5936
JmPotato <ghzpotato@gmail.com>
2013-08-21 13:38:04 +0800

    Update theme.py

--- theme.py
+++ theme.py
@@ -3,6 +3,5 @@ author = 'JmPotato'
 homepage = 'https://github.com/JmPotato/catsup-theme-brief'
 post_per_page = 3
 vars = {
-    "imge_path": "http://farm4.staticflickr.com/3752/9436685808_ff3fd6be7c_m.jpg",
     "description": "a blog"
 }
