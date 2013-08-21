56f6b58476c994159c2ff6b8e566316f05f5f0df
JmPotato <ghzpotato@gmail.com>
2013-08-21 16:23:29 +0800

    Fix a bug

--- filters.py
+++ filters.py
@@ -1,6 +1,5 @@
-import hashlib
-
 def gravatar(email):
+	import hashlib
 	size=48 * 2
 	hashed_email = hashlib.md5(email).hexdigest()
 	url = "http://9429127371.a.uxengine.net/avatar/" + hashed_email
